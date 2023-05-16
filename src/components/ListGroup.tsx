6dee2b9f3be12c446a0c6051a017cc7b7a40241b
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-16 14:59:39 +0530

    Added a list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,16 @@
-function ListGroup () {
+function ListGroup() {
+  const items = ["New York", "San Franciso", "Tokyo", "London", "Paris"];
 
-    return <h1>List Group</h1>;
-
+  return (
+    <>
+      <h1>List</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
