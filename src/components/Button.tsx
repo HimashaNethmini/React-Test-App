5da65f5ba72cefdec4c213ba4ba0fee4b0832739
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-16 19:02:36 +0530

    Added Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+
+
+interface Props {
+    children: string;
+    onClick: () => void;
+}
+
+
+const Button = ( {children, onClick }: Props) => {
+  return (
+    <button className="btn btn-danger"  onClick= {onClick} > {children} </button>
+    
+  );
+};
+
+export default Button;
