075b2bebc1b860d527c487ad8ce01419d3a24825
HimashaNethmini <himashanethmini05@gmail.com>
2023-05-16 13:56:37 +0530

    Initial setup files

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+//PascalCasing
+
+function Message(){
+
+    //JSX: Javascript XML
+    const name = '';
+
+    if (name) 
+        return <h1>Hello { name}</h1>;
+    return <h1>Hello love</h1>;
+
+}
+
+export default Message;
